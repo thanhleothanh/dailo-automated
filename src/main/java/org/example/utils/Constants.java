ab900c1140d5bc3da1b0aec5207869473b3e236b
thanhleothanh <thanhng260588@gmail.com>
2023-03-26 16:24:25 +0700

    wait time constant change to 15s, remove log4j2 file appender

--- src/main/java/org/example/utils/Constants.java
+++ src/main/java/org/example/utils/Constants.java
@@ -3,6 +3,6 @@ package org.example.utils;
 public class Constants {
   public static final String EXIT_KEYWORD = "EXIT";
   public static final String ADBLOCK_EXTENSION_PATH = "src/main/resources/extensions/adblock.crx";
-  public static final double DEFAULT_TIMEOUT = 30000.0;
+  public static final double DEFAULT_TIMEOUT = 15000.0;
 
 }
